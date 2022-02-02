8aea73d90db000a5a394351b7467beb89b6dde21
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-02 15:06:38 +0100

    complete unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -147,12 +147,11 @@ suite("UnitTests", () => {
     );
   });
   test("Valid puzzle strings pass the solver", () => {
-    assert.equal(
+    assert.isString(
       solver.solve(
         "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
       ),
-      "769235418851496372432178956174569283395842761628713549283657194516924837947381625",
-      "Solver should solve correctly"
+      "Solver should pass a solveable puzzle"
     );
   });
   test("Invalid puzzle strings fail the solver", () => {
@@ -164,6 +163,12 @@ suite("UnitTests", () => {
     );
   });
   test("Solver returns the expected solution for an incomplete puzzle", () => {
-    assert.fail();
+    assert.equal(
+      solver.solve(
+        "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
+      ),
+      "769235418851496372432178956174569283395842761628713549283657194516924837947381625",
+      "Solver should solve correctly"
+    );
   });
 });
