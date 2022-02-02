eccd118323aa9ba7be9aad7274bc93f851aa789c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-02 18:36:35 +0100

    Allow checks for squares that already have a number

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const SudokuSolver = require("../controllers/sudoku-solver.js");
+const { setCharAt } = require("../utils.js");
 
 module.exports = function (app) {
   let solver = new SudokuSolver();
@@ -12,25 +13,30 @@ module.exports = function (app) {
      * puzzle: "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."
      * value: "1"
      */
+    console.log(
+      `value: ${req.body.value}, coordinate: ${req.body.coordinate}, puzzle: ${req.body.puzzle}`
+    );
 
     if (!req.body.puzzle || !req.body.coordinate || !req.body.value) {
       return res.json({ error: "Required field(s) missing" });
     }
+
+    let puzzle = req.body.puzzle;
 
     const coords = solver.getRowAndColumnFromStringPosition(
       req.body.coordinate
     );
     if (!coords) return res.json({ error: "Invalid coordinate" });
 
-    if (!solver.validateLength(req.body.puzzle)) {
+    if (!solver.validateLength(puzzle)) {
       return res.json({ error: "Expected puzzle to be 81 characters long" });
     }
 
-    if (!solver.validateCharacters(req.body.puzzle)) {
+    if (!solver.validateCharacters(puzzle)) {
       return res.json({ error: "Invalid characters in puzzle" });
     }
 
-    if (!solver.validateBoardLegality(req.body.puzzle)) {
+    if (!solver.validateBoardLegality(puzzle)) {
       return res.json({ error: "Puzzle cannot be solved" });
     }
 
@@ -42,8 +48,16 @@ module.exports = function (app) {
       return res.json({ error: "Invalid value" });
     }
 
+    // if the input value is already on the board in that place, return true
+    // we've already checked that the puzzle is legal, so we know it's true
+    // get the numerical version of the coordinate
+    const coordinateNum = 9 * coords.row + coords.col;
+    // check if there is already something on spot
+    // if so, create a new puzzle that is empty in that spot, and check for that puzzle
+    puzzle = setCharAt(puzzle, coordinateNum, ".");
+
     const conflict = solver.checkConflict(
-      req.body.puzzle,
+      puzzle,
       coords.row,
       coords.col,
       req.body.value
